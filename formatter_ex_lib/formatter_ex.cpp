99a6cec438e18e1741ff5bace4d36e637a5d3737
sabirovatv <sabirovatamara77@gmail.com>
2026-06-07 21:39:10 +0300

    Update formatter_ex.cpp

--- formatter_ex_lib/formatter_ex.cpp
+++ formatter_ex_lib/formatter_ex.cpp
@@ -1,5 +1,6 @@
 #include "formatter_ex.h"
 #include "formatter.h"
+#include <ostream>
 
 std::ostream& formatter(std::ostream& out, const std::string& message) {
     return out << formatter(message);
